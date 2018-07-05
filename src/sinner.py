8c3aa868945c1c90445a68c58c251345aa31000f
SathusanK <sathusan.kan@gmail.com>
2018-07-04 23:16:31 -0400

    modified the file to accomodate for future changes and removed unnecessary attributes (for the time being)

--- src/sinner.py
+++ src/sinner.py
@@ -1,17 +1,11 @@
 class Sinner:
     
     def __init__(self, post, name, keyword):
-        self.post = post
         self.name = name
-        self.keyword = keyword
-    
-    def set_post(self, post):
-        self.post = post
-        return
-    
-    def get_post(self):
-        return self.post
-    
+        self.count = 0
+#         self.keyword = keyword
+#         self.post = post
+
     def set_name(self, name):
         self.name = name
         return
@@ -19,10 +13,28 @@ class Sinner:
     def get_name(self):
         return self.name
     
-    def set_keyword(self, keyword):
-        self.keyword = keyword
+    def set_count(self, count):
+        self.count = count
         return
     
-    def get_keyword(self):
-        return self.keyword
+    def get_count(self):
+        return self.count
+    
+    def add_count(self):
+        self.count += 1
+        return
+    
+#     def set_post(self, post):
+#         self.post = post
+#         return
+#     
+#     def get_post(self):
+#         return self.post
+    
+#     def set_keyword(self, keyword):
+#         self.keyword = keyword
+#         return
+#     
+#     def get_keyword(self):
+#         return self.keyword
     
