b666399a0992e747bdd29599adc5bb809e6e7ccb
SathusanK <sathusan.kan@gmail.com>
2018-07-04 23:18:36 -0400

    Imported pickle for object storage. Created 'update_count' function for future implementation. Fixed a bug in srch that prevented it from sleeping. Also updated documentation

--- src/functions.py
+++ src/functions.py
@@ -1,5 +1,6 @@
 # Built-in libraries
 # import time
+import pickle
 
 #External libraries for Reddit use
 # import praw
@@ -8,7 +9,17 @@
 # Personal scripts and files
 # import sinner
 
-def check(author, check_value):
+def check(author, value_exists):
+    '''
+    Iterates through all the sinners and adds them to the list if they do not exist.
+    
+    Returns whether or not the sinner is a repeat offender (variable: value_exists)
+    
+    Variables:
+    sinners - text files of sinners
+    sinner_list - array of sinners
+    value_exists - boolean value of whether or not the sinner exists in the txt file
+    '''
     sinners = open('sinners.txt', "r+")
                     
     sinner_list = []
@@ -18,37 +29,56 @@ def check(author, check_value):
         
     if str(author) not in sinner_list:
         sinners.write(str(author) + '\n')
-        check_value = True
+        value_exists = True
 
     sinners.close()
-    return check_value
+    
+    return value_exists
+
+def update_count(author):
+    '''
+    Updates the sin count of the author
+    **IMPORTANT** In this context, the author variable is an object
+    
+    Variables:
+    author - author object from Reddit API
+    '''
+    return
 
 def srch(subreddit, KEYWORDS):
     '''
     Checks the 20 "hot" submissions in a subreddit for a keyword.
     Then, it will send the data to another function where a sinner
     class will be created and user data is stored.
+    
+    Variables:
+    value_exists - boolean value of whether or not the sinner exists in the txt file
+    author - string of the author's username
     '''
     
-    check_value = False
+    value_exists = False
     
     for submission in subreddit.hot(limit=20):
         submission.comments.replace_more(limit=None)
         submission.comments_sort = 'hot'
-
         for comment in submission.comments.list():
             author = comment.author
             split_comment = comment.body.lower().split(' ')
             
+#             print(author)
+#             print(split_comment)
+            
             for word in split_comment:
                 if word in KEYWORDS:
-                    check_value = check(author, check_value)
+                    value_exists = check(author, value_exists)
                     print(author)
                     print(split_comment)
                     
-                    if check_value == False:
+                    if value_exists == False:
                         continue
                     else:
-                        break
-                    
+#                         update_count(comment.author) #function that checks the sin count
+                        
+                        return
+            
     return
