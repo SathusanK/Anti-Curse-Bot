ca26f33511a616b9c33452021164c1b549409a94
SathusanK <sathusan.kan@gmail.com>
2018-07-13 00:14:16 -0400

    Slightly changed the message

--- src/message.py
+++ src/message.py
@@ -1,5 +1,6 @@
 # Work in progress
 
 reply_msg = '''
-You have now been banned from a Christian Minecraft server.
+You have now been banned from a Christian Minecraft server for saying %WORD%.
+You have been offensive %count% time(s).
 '''#.replace
