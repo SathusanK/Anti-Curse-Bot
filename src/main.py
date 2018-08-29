4ee605d70bf9437f90708062c568ae7021d3b111
Sathu <Sathu@DESKTOP-E4ES33B.lan>
2018-08-28 21:34:49 -0400

    Decreased time.sleep from 10 seconds to 1 second to speed up bug fixing.

--- src/main.py
+++ src/main.py
@@ -16,11 +16,13 @@ reddit = praw.Reddit(client_id = config.client_id,
                      user_agent = config.user_agent)
 
 # Subreddit initializations
-subreddit_name = 'all'
+# subreddit_name = 'all'
+subreddit_name = 'pythonforengineers'
 subreddit = reddit.subreddit(subreddit_name)
 
-KEYWORDS = ['heck', 'frick', 'darn', 'and'] #Temporarily added "and" to speed up testing
+KEYWORDS = ['heck', 'frick', 'darn', 'h**k', 'f***', 's**t', 'and'] #Temporarily added "and" to speed up testing
 
 while True:
     srch(subreddit, KEYWORDS)
-    time.sleep(10)
+#     time.sleep(10)
+    time.sleep(1)
