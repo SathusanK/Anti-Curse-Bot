af9a8109ced991fb4e707af489c860482f326ca6
Sathu <Sathu@192.168.2.61>
2019-01-03 15:08:44 -0500

    Cleaned up the file

--- src/main.py
+++ src/main.py
@@ -9,20 +9,18 @@ import config
 from functions import srch
 
 # Reddit instace initialization
-reddit = praw.Reddit(client_id = config.client_id,
-                     client_secret = config.client_secret,
-                     username = config.username,
-                     password = config.password,
-                     user_agent = config.user_agent)
+reddit = praw.Reddit(client_id=config.client_id,
+                     client_secret=config.client_secret,
+                     username=config.username,
+                     password=config.password,
+                     user_agent=config.user_agent)
 
 # Subreddit initializations
-# subreddit_name = 'all'
-subreddit_name = 'pythonforengineers'
+subreddit_name = 'dankmemes'
 subreddit = reddit.subreddit(subreddit_name)
 
-KEYWORDS = ['heck', 'frick', 'darn', 'h**k', 'f***', 's**t', 'and'] #Temporarily added "and" to speed up testing
+KEYWORDS = ['heck', 'frick', 'darn', 'h**k', 'f***', 's**t']
 
 while True:
     srch(subreddit, KEYWORDS)
-#     time.sleep(10)
-    time.sleep(1)
+    time.sleep(601)  # 10 min., 1 sec due to Reddit restrictions
